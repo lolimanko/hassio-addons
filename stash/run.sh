ce8bb4ecaa4a5a4447b02b5c76d197bac670628b
lolimanko <138034338+lolimanko@users.noreply.github.com>
2026-04-28 10:19:06 +0800

    hjhh

--- stash/run.sh
+++ stash/run.sh
@@ -1,3 +1,26 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/sh
 
-exec /stash-linux-arm64v8
+if [ -f "/root/.stash/config.yml" ]; then
+    
+    echo "config.yml exist."
+
+else
+    echo "config.yml not exist."
+    cp /config.yml /root/.stash/config.yml
+fi
+
+if grep -q 'jwt_secret_key' '/root/.stash/config.yml'; then
+  echo "Found jwt_secret_key."
+else
+  echo "not Found jwt_secret_key."
+  echo 'jwt_secret_key: '$(openssl rand -hex 32) >> '/root/.stash/config.yml'
+  
+fi
+if grep -q 'session_store_key' '/root/.stash/config.yml'; then
+  echo "Found session_store_key."
+else
+  echo "not Found session_store_key."
+  echo 'session_store_key: '$(openssl rand -hex 32) >> '/root/.stash/config.yml'
+  
+fi
+exec stash
