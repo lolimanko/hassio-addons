19d31e4999b2a4f8781a83669a4eeacb9bef18ae
lolimanko <138034338+lolimanko@users.noreply.github.com>
2026-08-05 09:09:11 +0800

    pg

--- stash/run.sh
+++ stash/run.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bashio
+
+#INGRESS_PATH="$(bashio::app.ingress_entry)"
+ssl=$(bashio::config 'ssl')
+
+certfile=$(bashio::config 'certfile')
+
+keyfile=$(bashio::config 'keyfile')
+ingress_port=$(bashio::app.ingress_port)
+ingress_interface=$(bashio::app.ip_address)
+sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/http.d/ingress.conf
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/http.d/ingress.conf
 
 if [ -f "/root/.stash/config.yml" ]; then
     
@@ -8,7 +19,17 @@ else
     echo "config.yml not exist."
     cp /config.yml /root/.stash/config.yml
 fi
-
+if [ $ssl ]; then
+    
+    echo "ssl enable."
+    cp /ssl.conf /etc/nginx/http.d/ssl.conf
+    sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/http.d/ssl.conf
+    sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/http.d/ssl.conf
+else
+    echo "ssl disable."
+    #cp /config.yml /root/.stash/config.yml
+fi
+#sed -i "s|external_host:|external_host: https://buvi.duckdns.org:8123$INGRESS_PATH|g" /root/.stash/config.yml
 if grep -q 'jwt_secret_key' '/root/.stash/config.yml'; then
   echo "Found jwt_secret_key."
 else
@@ -23,7 +44,6 @@ else
   echo 'session_store_key: '$(openssl rand -hex 32) >> '/root/.stash/config.yml'
   
 fi
-
 if [ -d /root/.stash/generated ]; then
   echo "generated Yes"
 else
@@ -42,4 +62,7 @@ else
   echo "blobs No"
   mkdir /root/.stash/blobs
 fi
-exec stash
+
+nginx -g "daemon off;error_log /dev/stdout debug;" & exec stash
+
+
