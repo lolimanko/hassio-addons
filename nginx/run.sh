283ee64f2773b287d2f0fcd9427d12b5bacd48e7
srwfe <root@rpi4.localdomain>
2026-08-09 23:59:21 +0800

    pg

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/with-contenv bashio
-FILE="/config/default.conf"
+DIR="/config/nginx"
 
-if [ -f "$FILE" ]; then
-    cp -f /config/default.conf /etc/nginx/http.d/
-    echo "File $FILE exists and is a regular file."
+if [ -d "$DIR" ]; then
+    cp -r /config/nginx /etc
+    echo "File $DIR exists and is a regular file."
 else
-    echo "File $FILE does not exist or is not a regular file."
+    echo "File $DIR does not exist or is not a regular file."
 fi
 nginx -g "daemon off;error_log /dev/stdout debug;"
