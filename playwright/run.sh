c0cfad184b760d55ee6b013c278fccb7a570b880
srwfe <root@rpi4.localdomain>
2026-08-12 23:48:19 +0800

    s6

--- playwright/run.sh
+++ playwright/run.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/with-contenv bashio
-
-FILE="/app/playwright_ha.py"
+FILE="/config/playwright_ha.py"
 
 if [ -f "$FILE" ]; then
+    #cp -f /app/default.conf /etc/nginx/http.d/
     echo "File $FILE exists and is a regular file."
-
-    chmod 777 /app/playwright_ha.py
-
-    exec python3 /app/playwright_ha.py
+    chmod 777 /config/playwright_ha.py
+    exec python3 /config/playwright_ha.py
 else
     echo "File $FILE does not exist or is not a regular file."
 fi
-#nginx -g "daemon off;error_log /dev/stdout debug;"
+
 
